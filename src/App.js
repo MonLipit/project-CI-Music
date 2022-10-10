dee58f02c96eac9e483e3b934ea09fc3f028bb0a
MonLipit <tranhieu199824@gmail.com>
2022-10-10 21:39:23 +0700

    i yarn

--- src/App.js
+++ src/App.js
@@ -1,45 +1,42 @@
-import {useState,useEffect} from 'react'
-
-import './App.scss';
+import { useState, useEffect } from "react";
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.scss";
 
 // import HeroSection from './component/HeroSection/HeroSection';
-import 'swiper/swiper.min.css';
+import "swiper/swiper.min.css";
 
-import Navbar from './component/Navbar/Navbar';
-import NavbarMobile from './component/NavbarMobile/NavbarMobile';
+import Navbar from "./component/Navbar/Navbar";
+import NavbarMobile from "./component/NavbarMobile/NavbarMobile";
 
-import MusicPlayer from './component/MusicPlayer/MusicPlayer';
-import MusicPlayerMobile from './component/MusicPlayerMobile/MusicPlayerMobile';
+import MusicPlayer from "./component/MusicPlayer/MusicPlayer";
+import MusicPlayerMobile from "./component/MusicPlayerMobile/MusicPlayerMobile";
 
-import Router from './Router/Router';
+import Router from "./Router/Router";
 
-import Footer from './component/Footer/Footer';
-
+import Footer from "./component/Footer/Footer";
 
 function App() {
+  const [show, setShow] = useState(false);
+  let screenWidth = window.innerWidth;
 
-  const [show,setShow] = useState(false)
-  let screenWidth = window.innerWidth
+  useEffect(() => {
+    console.log(screenWidth);
+    if (screenWidth < 1280) {
+      setShow(true);
+    }
+  }, [screenWidth]);
 
-  useEffect(()=>{
-      console.log(screenWidth)
-      if(screenWidth < 1280){
-        setShow(true)
-      }
-  },[screenWidth])
- 
   return (
-      <div className="App">
-          <Navbar/>
-          <NavbarMobile/>
-          <div className="content">
-            <Router/>
-            <Footer/>
-          </div>
-          {show?<MusicPlayerMobile/>:<MusicPlayer/>}
+    <div className="App">
+      <Navbar />
+      <NavbarMobile />
+      <div className="content">
+        <Router />
+        <Footer />
       </div>
+      {show ? <MusicPlayerMobile /> : <MusicPlayer />}
+    </div>
   );
 }
-
 
 export default App;
